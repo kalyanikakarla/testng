899f764ef92a74e8afa9e05044ef369f84ce23b7
kakarlakalyani <kakarlakalyani123@gmail.com>
2023-02-20 15:42:06 +0530

    demo file adding

---
+++ src/test/java/com/kal/Demo.java
@@ -0,0 +1,16 @@
+package com.kal;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+    @Test
+    public void menu()
+    {
+        System.out.println("sample program");
+    }
+
+
+
+
+}
