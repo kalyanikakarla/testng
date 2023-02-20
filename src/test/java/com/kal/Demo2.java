5e4b235fabe9c0a0841d6ed0b46ad33c417504b9
kakarlakalyani <kakarlakalyani123@gmail.com>
2023-02-20 15:58:09 +0530

    file added

---
+++ src/test/java/com/kal/Demo2.java
@@ -0,0 +1,12 @@
+package com.kal;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+    @Test
+    public void set()
+    {
+        System.out.println("kalyani");
+    }
+
+}
---
+++ src/test/java/com/kal/Demo3.java
@@ -0,0 +1,12 @@
+package com.kal;
+
+import org.testng.annotations.Test;
+
+public class Demo3 {
+    @Test
+    public void setup()
+    {
+        System.out.println("selenium");
+    }
+
+}
